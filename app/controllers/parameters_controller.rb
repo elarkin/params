0330616aadb9e4c2ca93b690105102fc7dec5070
Evan Larkin <evan.larkin@vibes.com>
2013-03-14 14:34:03 -0500

    Now rendering as text/plain

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,6 +1,6 @@
 require 'pp'
 class ParametersController < ApplicationController
   def index
-    render :text => params.pretty_inspect
+    render :text => params.pretty_inspect, :content_type => "text/plain"
   end
 end
